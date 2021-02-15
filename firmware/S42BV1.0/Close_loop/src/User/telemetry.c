eea33644a9e51fcf1db3b83e90de9f2c2e8fb619
strelok.vc <strelok.vc@gmail.com>
2021-02-15 15:06:08 +0300

    Added angle error to telemetry stream.

--- firmware/S42BV1.0/Close_loop/src/User/telemetry.c
+++ firmware/S42BV1.0/Close_loop/src/User/telemetry.c
@@ -8,16 +8,23 @@
 
 uint32_t prevStreamTickCount;
 
+// Stream Angle and AngleError to serial port
 void StreamAngle()
 {
   if (tickCount < (prevStreamTickCount + 5))
     return;
 
-  struct Serial_Msg_Angle streamedAng;
-  streamedAng.angle = (float)y;
-
+  struct Serial_Msg_Angle streamAngle;
   // Use 'y' which is read in the controller loop
-  uint8_t len = Serial_GeneratePacket(SERIAL_MSG_ANGLE, &streamedAng, sizeof(streamedAng));
+  streamAngle.angle = (float)y;  
+  
+  uint8_t len = Serial_GeneratePacket(SERIAL_MSG_ANGLE, (uint8_t *)&streamAngle, sizeof(streamAngle));
+  UART1_Write(packetBuffer, len);
+
+  struct Serial_Msg_AngleError ae;
+  ae.error = pid_error * 0.021972;
+
+  len = Serial_GeneratePacket(SERIAL_MSG_ANGERROR, (uint8_t *)&ae, sizeof(ae));
   UART1_Write(packetBuffer, len);
 
   prevStreamTickCount = tickCount;
